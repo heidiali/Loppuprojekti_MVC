406ec7bc4e3f656c96b28cf25c41ffde7e101a90
heidiali <heidi.alikippari@gmail.com>
2020-09-20 23:40:03 +0300

    Cleaning

--- Loppuprojekti_MVC/Controllers/HomeController.cs
+++ Loppuprojekti_MVC/Controllers/HomeController.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Loppuprojekti_MVC.Models;
-using Microsoft.AspNetCore.Mvc.Localization;
 
 namespace Loppuprojekti_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly IHtmlLocalizer<HomeController> _localizer;
-
-        //public HomeController(HtmlLocalizer<HomeController> localizer)
-        //{
-        //    _localizer = localizer;
-        //}
 
         public IActionResult Index(string searchTerms)
         {
@@ -41,20 +33,5 @@ namespace Loppuprojekti_MVC.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        //TODO: remove, unused
-        //[HttpGet]
-        //public ActionResult NSearch(NameSearch searchTerms)
-        //{
-        //    if (string.IsNullOrEmpty(searchTerms.ScientificName))
-        //    {
-        //        ModelState.AddModelError("", "Please search with a name.");
-        //        return RedirectToAction("Index");
-        //    }
-        //    else
-        //    {
-        //        return RedirectToAction("SingleSpecies","Species");
-        //    }
-
-        //}
     }
 }
